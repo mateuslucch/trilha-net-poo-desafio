71b1a15aca2b8b0c115fc1470ced96acc78e159d
Mateus A Lucchese <mateuslucch@gmail.com>
2022-12-16 21:01:38 -0300

    desafio-poo. 1st commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia1 = new Nokia("55 81165555", "Tijolão", "6666699", 64000);
+Iphone iphone1 = new Iphone("555 66666", "Oquecustaumrim4Pro", "777882", 128000);
+
+nokia1.Ligar();
+nokia1.InstalarAplicativo("Zapzap");
+nokia1.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("DioApp");
